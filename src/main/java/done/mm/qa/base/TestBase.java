0c8e4a70a620d801f201ce9e5c64bd84b49cce8b
Rushikesh Bhadoria <92309344+rushikesh2407@users.noreply.github.com>
2021-11-07 23:09:00 +0530

    Update TestBase.java

--- src/main/java/done/mm/qa/base/TestBase.java
+++ src/main/java/done/mm/qa/base/TestBase.java
@@ -40,10 +40,10 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "/ok/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "/ok");
 			driver = new ChromeDriver();
 		} else {
-			System.setProperty("webdriver.chrome.driver", "/ok/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "/ok");
 			driver = new ChromeDriver();
 		}
 		e_driver = new EventFiringWebDriver(driver);
