39a627f29675cad10c67ae368b022dcd3d4a9845
Rushikesh Bhadoria <92309344+rushikesh2407@users.noreply.github.com>
2021-11-09 23:51:32 +0530

    Update TestBase.java

--- src/main/java/done/mm/qa/base/TestBase.java
+++ src/main/java/done/mm/qa/base/TestBase.java
@@ -43,9 +43,12 @@ public class TestBase {
 			WebDriverManager.firefoxdriver().browserVersion("91.0.2").setup();
 			 driver = new FirefoxDriver();
 		} else {
+			WebDriverManager.chromedriver().browserVersion("93.0.4577.63").setup();
+			 driver = new ChromeDriver();
+// 			WebDriverManager.firefoxdriver().browserVersion("91.0.2").setup();
+// 			 driver = new FirefoxDriver();
 			
-			WebDriverManager.firefoxdriver().browserVersion("91.0.2").setup();
-			 driver = new FirefoxDriver();
+			
 		}
 		e_driver = new EventFiringWebDriver(driver);
 		// Now create object of EventListerHandler to register it with EventFiringWebDriver
