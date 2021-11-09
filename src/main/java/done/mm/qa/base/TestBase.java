cbb67b75df3ca35926721fcc7a64744b51b84b18
Rushikesh Bhadoria <92309344+rushikesh2407@users.noreply.github.com>
2021-11-09 12:17:55 +0530

    Update TestBase.java

--- src/main/java/done/mm/qa/base/TestBase.java
+++ src/main/java/done/mm/qa/base/TestBase.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import done.mm.qa.util.TestUtil;
 import done.mm.qa.util.WebEventListener;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class TestBase {
 
 	public static WebDriver driver;
